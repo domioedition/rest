cf27d97d00aaf83168bc2922d3af7ce009ff33da
domioedition <domioedition@gmail.com>
2017-10-30 00:19:05 +0200

    Added methods post, delete

--- app/routes.php
+++ app/routes.php
@@ -11,14 +11,64 @@ $app->put('/task/{id}', 'ControllerTask:updateById');
 $app->delete('/task/{id}', 'ControllerTask:DeleteById');
 
 $app->get('/projects', function ($request, $response) {
+  try{
+       $con = $this->db;
+       $sql = "SELECT * FROM projects";
+       $result = null;
+       foreach ($con->query($sql) as $row) {
+           $result[] = $row;
+       }
+       if($result){
+           return $response->withJson(array('status' => 'true','result'=>$result),200);
+       }else{
+           return $response->withJson(array('status' => 'Users Not Found'),422);
+       }
 
-    echo "All projects";
-
-    //$mapper = new TicketMapper($this->db);
-    //$tickets = $mapper->getTickets();
-
-    //$response->getBody()->write(var_export($tickets, true));
-//    return $response;
+   }
+   catch(\Exception $ex){
+       return $response->withJson(array('error' => $ex->getMessage()),422);
+   }
 });
 
 
+$app->post('/projects', function ($request, $response) {
+  try{
+      $data = $request->getParsedBody();
+      $project_data = array();
+      $project_data['project_user_id_creator'] = filter_var($data['project_user_id_creator'], FILTER_SANITIZE_STRING);
+      $project_data['project_name'] = filter_var($data['project_name'], FILTER_SANITIZE_STRING);
+      $project_data['project_description'] = filter_var($data['project_description'], FILTER_SANITIZE_STRING);
+      $sql = "INSERT INTO projects (project_user_id_creator, project_name, project_description) VALUES ('".$project_data['project_user_id_creator']."','". $project_data['project_name']."','". $project_data['project_description']."')";
+      $con = $this->db;
+      $result = null;
+      $result = $con->query($sql);
+      if($result){
+        return $response->withJson(array('status' => 'true','result'=>$project_data),200);
+      }else{
+        return $response->withJson(array('status' => 'Project not added','result'=>$project_data),422);
+      }
+   }
+   catch(\Exception $ex){
+       return $response->withJson(array('error' => $ex->getMessage()),422);
+   }
+});
+
+$app->delete('/projects/{id}', function ($request, $response) {
+  try{
+      $id = $request->getAttribute('id');
+      $con = $this->db;
+      $sql = "DELETE FROM projects WHERE id = :id";
+      $pre  = $con->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+      $values = array(
+      ':id' => $id);
+      $result = $pre->execute($values);
+      if($result){
+         return $response->withJson(array('status' => 'Project Deleted'),200);
+      }else{
+         return $response->withJson(array('status' => 'Project Not Found'),422);
+      }
+   }
+   catch(\Exception $ex){
+       return $response->withJson(array('error' => $ex->getMessage()),422);
+   }
+});
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,15 +5,15 @@ session_start();
 require __DIR__ . '/../vendor/autoload.php';
 
 
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
 
-$app = new \Slim\App(
-    [
-        'settings' => [
-            'displayErrorsDetails' => true,
-            'addContentLengthHeader' => false
-        ]
-    ]
-);
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "";
+$config['db']['dbname'] = "controlmyprojects";
+
+$app = new \Slim\App(["settings" => $config]);
 
 
 $container = $app->getContainer();
@@ -47,10 +47,13 @@ $container['ControllerTask'] = function($container){
     return new \App\Controllers\ControllerTask($container);
 };
 
+$container['ControllerProject'] = function($container){
+    return new \App\Controllers\ControllerProject($container);
+};
+
 //$container['ControllerProject'] = function($container){
 //    return new \App\Controllers\ControllerProject($container);
 //};
 
 
 require __DIR__ . '/../app/routes.php';
-
